342cec09024990b8ccaa2543c3fc91c7c2a74fbe
Harry MacCode <emarh.harry.code@gmail.com>
2024-02-14 08:25:34 +0100

    Create Server and Connect to MongoDB

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+const PORT = process.env.PORT || 4000;
+// process.env.MONGO_URI
+// console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Database connected to ${PORT}`);
+        })
+    })
+    .catch((err) => console.log(err));
